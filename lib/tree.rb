0cc40f89dbea79f9fad0d887728ac845a9816881
4R1u <62248461+4R1u@users.noreply.github.com>
2024-12-11 13:15:11 +0400

    Implement Tree#build_tree

--- lib/tree.rb
+++ lib/tree.rb
@@ -5,6 +5,17 @@ require_relative 'node'
 # Balanced Binary Search Tree
 class Tree
   def initialize(arr)
-    @root = build_tree(arr)
+    @root = build_tree(arr.uniq.sort)
+  end
+
+  def build_tree(arr)
+    return Node.new arr[0] if arr.length <= 1
+
+    mid = arr.length / 2
+    root = Node.new arr[mid]
+
+    root.left = build_tree(arr[..mid - 1])
+    root.right = build_tree(arr[mid + 1..])
+    root
   end
 end
